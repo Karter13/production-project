88fd215e4106230c777c65c17f74ed149aafa795
Helgi Bergmann <helgi.bergmann@outlook.com>
2023-02-15 21:24:20 +0100

    initial commit

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,15 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+    //@ts-ignore
+    const filterMods = Object.entries(mods)
+        .filter(([className, value]) => Boolean(value))
+        .map(([className]) => className)
+
+    return [
+        cls,
+        ...additional,
+        ...filterMods
+    ].join(' ');
+
+}
---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    //@ts-ignore
+    // ТАК НЕ ДЕЛАТЬ!!!!!!!!!!!ВРЕМЕННО
+    setTimeout(() => resolve( import('./AboutPage')), 2000)
+}));
