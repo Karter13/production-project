12585cc8a77436b355f5446dc87c24f8ae4a48ca
Helgi Bergmann <helgi.bergmann@outlook.com>
2023-02-22 21:49:55 +0100

    add storybook, add stories for Button and Sidebar

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,8 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): any {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -28,29 +28,7 @@ export function buildLoaders({ isDev }: BuildOptions): any {
         },
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => (
-                            Boolean(resPath.includes('.module.'))
-                        ),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     // если не используется тайпскрипт - нужен babel-loader для обработки jsx
     // ts-loader - обрабатывает jsx
--- src/widgets/NavBar/ui/Navbar.tsx
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -1,26 +1,32 @@
-import React, {FC} from 'react';
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './Navbar.module.scss'
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import React, { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
+import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar: FC<NavbarProps> = ({className}) => {
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+    const { t } = useTranslation();
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
 
             <div className={cls.links}>
                 <AppLink
-                    to={'/'}
+                    to="/"
                     theme={AppLinkTheme.SECONDARY}
                     className={cls.mainLink}
-                >Main</AppLink>
+                >
+                    {t('Главная')}
+                </AppLink>
                 <AppLink
-                    to={'/about'}
+                    to="/about"
                     theme={AppLinkTheme.SECONDARY}
-                >About site</AppLink>
+                >
+                    {t('О сайте')}
+                </AppLink>
             </div>
 
         </div>
