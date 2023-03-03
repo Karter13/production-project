3a819966682edc33a55cf7597b2beed9efff5178
Helgi Bergmann <helgi.bergmann@outlook.com>
2023-03-03 12:40:57 +0100

    add pre commit, fix screenshots new

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,16 +1,27 @@
-import { FC } from 'react';
+import { memo, useCallback } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { useDispatch } from 'react-redux';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string
 }
 
-export const LoginForm: FC<LoginFormProps> = ({ className }) => {
+export const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
@@ -19,13 +30,16 @@ export const LoginForm: FC<LoginFormProps> = ({ className }) => {
                 type="text"
                 className={cls.input}
                 placeholder={t('Введите имя пользователя')}
+                onChange={onChangeUsername}
             />
             <Input
                 type="text"
                 className={cls.input}
                 placeholder={t('Введите пароль')}
+                onChange={onChangePassword}
             />
             <Button
+                theme={ButtonTheme.OUTLINE}
                 type="submit"
                 className={cls.loginBtn}
             >
@@ -33,4 +47,4 @@ export const LoginForm: FC<LoginFormProps> = ({ className }) => {
             </Button>
         </div>
     );
-};
+});
